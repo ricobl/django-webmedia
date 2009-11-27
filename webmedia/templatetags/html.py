c3d2d3460b790357c8b935f537071a0c02adf225
Enrico <enrico@orangotoe.com.br>
2009-11-27 11:14:33 -0200

    Removido código para iniciar refatoração

--- webmedia/templatetags/html.py
+++ webmedia/templatetags/html.py
@@ -7,11 +7,8 @@ import time
 from django import template
 from django.conf import settings
 from django.utils.safestring import mark_safe, SafeUnicode
-from django.db.models.fields.files import ImageFieldFile
-from django import forms
 
-from quick_tag import quick_tag
-from utils import fs, imaging
+from ot.template import quick_tag
 
 register = template.Library()
 
@@ -61,133 +58,15 @@ def nocache(path):
 
 ## TAGS
 
-def html_image(context, src, width = None, height = None, method='crop', quality=80, **kwargs):
-    """
-    Função para lidar com imagens.
-    Faz o redimensionamento automático de imagens locais. 
-    """
-
-    # Precisa ter um caminho local
-    if not (src and src.startswith('/')):
-        return kwargs
-    
-    # Remove a URL base de media
-    clean_path = src.replace(settings.MEDIA_URL, '', 1)
-    # Caminho físico do arquivo
-    path = os.path.join(settings.MEDIA_ROOT, clean_path)
-    
-    img = None
-    
-    # Só processar o arquivo se alguma dimensão foi passada
-    if width or height:
-        
-        # Marca o nome de arquivo de acordo com o método (crop ou fit)
-        extra = ((method == 'crop') and '_c_') or '_t_'
-        
-        # Parâmetros para a função de redimensionamento
-        kw = {}
-
-        if width:
-            extra += 'w%s' % width
-            kw['max_width'] = width
-        if height:
-            extra += 'h%s' % height
-            kw['max_height'] = height
-
-        # Cria o nome da imagem redimensionada:
-            # 100x50 crop em "1.jpg": 1_c_w100h50.jpg
-            # 200x120 fit em "2.jpg": 2_t_w200h120.jpg
-            # 100 width fit em "3.jpg": 3_c_w200.jpg
-        clean_path = fs.add_to_basename(clean_path, extra)
-    
-        # Separa nome base e extensão
-        basepath, ext = os.path.splitext(clean_path)
-        # Extensão em minúsculas
-        ext = ext.lower()
-        
-        # Converte BMPs para PNGs, para economizar espaço e banda
-        if ext == '.bmp':
-            clean_path = basepath + '.png'
-            
-        # Caminho completo para o novo arquivo
-        kw['save_as'] = os.path.join(settings.MEDIA_ROOT, clean_path)
-    
-        # Só processa o arquivo se não existir
-        # ou se o destino é mais antigo do que o original 
-        if not os.path.isfile(kw['save_as']) or (os.path.getmtime(path) >= os.path.getmtime(kw['save_as'])):
-            # Redimensiona e guarda o objeto
-            img = imaging.resize_file(path, quality=quality, method=method, **kw)
-        # Caso contrário, apenas carrega a imagem  
-        else:
-            img = imaging.image_open(kw['save_as'])
-    
-        context['src'] = '/'.join((settings.MEDIA_URL, clean_path)).replace('//', '/')
-        
-    # Nenhum processamento feito?
-    else:
-        # Carrega a imagem
-        img = imaging.image_open(path)
-    
-    # Atualiza dimensões da imagem no contexto
-    context['width'], context['height'] = img.size
-    
-    return kwargs
-
-def html_flash(context, src, **kwargs):
-    context['wmode'] = kwargs.pop('wmode', 'opaque')
-    return kwargs
-
-# Funções customizadas para cada extensão
-custom_html_media = {
-    'bmp': html_image,
-    'gif': html_image,
-    'jpeg': html_image,
-    'jpg': html_image,
-    'png': html_image,
-    'swf': html_flash,
-}
-
 @register.tag
 @quick_tag
-def html_media(context, src, get = '', **kwargs):
-    """
-    Tag para centralizar como é feita a exibição de elementos html.
-    Cada elemento é renderizado com um template de acordo com a extensão do objeto.
-
-    Exemplo de uso:
-    {% load html %}
-    {% html_media 'pic.jpg' width=50 height=100 %}
-    """
+def embed(context, src, **kwargs):
     # Falha silenciosamente se não houver um caminho
     if not src:
         return ''
 
-    #se o src for um ImageFieldFile
-    if isinstance(src,ImageFieldFile):
-        src = src.name
-
-    # Se o caminho não for externo ou absoluto, prefixa com o `MEDIA_URL`
-    
-    if not src.startswith('http'):
-        if not src.startswith('/'):
-            src = settings.MEDIA_URL + src
-            
-        # Falha silenciosamente se o arquivo local não existir
-        if not os.path.isfile(url_to_root(src)):
-            return ''
-            
-        # Acrescenta a data de modificação ao query-string
-        get += (get and '&' or '') + nocache(src)
-
-    # Adiciona '?' se houver um query-string
-    get = (get and '?' or '') + get
-    
     # Quebra o caminho e a extensão
-    try:
-        src_no_ext, ext = src.rsplit('.', 1)
-    except ValueError:
-        src_no_ext = src
-        ext = ''
+    src_no_ext, ext = os.path.splitext(src)
     
     # Contexto para o objeto a ser renderizado
     object_context = kwargs.copy()
@@ -222,86 +101,4 @@ def html_media(context, src, get = '', **kwargs):
     object_context['attrs'] = mark_safe(str_attrs)
     
     return template.loader.render_to_string('utils/html/%s.html' % ext.lower(), object_context)
-
-@register.tag
-@quick_tag
-def html_image_resize(context, src, **kwargs):
-    """
-    Função redimensionar imagens.
-    Faz o redimensionamento automático de imagens locais. 
-    """
-    
-    if not ((src and src.startswith('/')) or (width or height)):
-        return src
-    
-    html_image(context, src, **kwargs)
-    
-    return context['src']
-
-@register.tag
-@quick_tag
-def html_form_field(context, f, type='field', **kwargs):
-    
-    field = f.field
-    
-    for att, value in kwargs.items():
-        field.widget.attrs[att] = value
-    
-    object_context = {'field': f}
-    
-    return template.loader.render_to_string(('utils/html_forms/%s.html' % type,
-                                             'utils/html_forms/field.html'), object_context)
-
-# FILTERS
-
-@register.filter
-def number_format(num, places=0):
-    """Format a number according to locality and given places"""
-    import locale
-    locale.setlocale(locale.LC_ALL, "")
-    return locale.format("%.*f", (places, num), True)
-
-
-@register.filter
-def remove_tag_blocks(value, taglist):
-    """ Remove um bloco inteiro das tags na lista separada por espaço, ex: 
-    texto|remove_tag_block:"table"
-    <table class="table"><tr><td>Este texto será removido</td></tr></table>
-    """
-    
-    tags = '|'.join(taglist.split())
-    
-    # Regex para remover tags com conteúdo <tag></tag>
-    re_full_tags = re.compile('<(%s)[^>]*?>.*?</\\1>' % tags, re.S)
-    # Regex para remover tags simples <tag />
-    re_auto_tags = re.compile('<(%s)[^>]*?/?>' % tags, re.S)
-    
-    value = re_full_tags.sub('', value)
-    value = re_auto_tags.sub('', value)
-    
-    return value
-
-@register.tag
-@quick_tag
-def query_string(context, **kwargs):
-    """
-    Extrai a query-string do contexto e atualiza as variáveis
-    para formar um novo string.
-    """
-    req_get = context.get('request').GET.copy()
-    # Usando setitem em cada chave
-    # um update geral pode duplicar itens
-    for k in kwargs:
-        req_get.__setitem__(k, kwargs[k])
-    return req_get.urlencode()
-
-# Expressão regular para encontrar blocos de comentários condicionais
-RE_CONDITIONAL_COMMENTS = re.compile(r'<!--\[if [^]]*?\]>.*?<!\[endif\]-->', re.S)
-
-@register.filter
-def remove_cc(text):
-    """
-    Remove comentários condicionais de um texto.
-    """
-    return RE_CONDITIONAL_COMMENTS.sub('', text)
 
